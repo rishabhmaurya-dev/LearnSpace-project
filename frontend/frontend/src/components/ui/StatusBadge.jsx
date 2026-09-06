8b0c5124363b26eaf81070d49fb398a0b1756983
Rishabh Maurya <rishabhmaurya8460@gmail.com>
2026-09-07 00:06:30 +0530

    initial commit

--- frontend/frontend/src/components/ui/StatusBadge.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./StatusBadge.module.css";
-
-const StatusBadge = ({ tone = "neutral", children }) => {
-  return <span className={`${styles.badge} ${styles[tone]}`}>{children}</span>;
-};
-
-export default StatusBadge;
